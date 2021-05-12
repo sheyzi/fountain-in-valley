e98e6bf0ceeb6473abb40ff8b11c66d49d5d2503
sheyziSilver <sheyzilife.info@gmail.com>
2021-05-12 12:28:46 -0700

    Registration

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,3 +5,7 @@ auth = Blueprint('auth', __name__)
 @auth.route('/login/', methods=['GET', 'POST'])
 def login():
     return render_template('auth/login.html')
+
+@auth.route('/register/', methods=['GET', 'POST'])
+def register():
+    return render_template('auth/register.html')
